300c7f06375b5b2f69033551159ee8e2999959b3
smileyle <smileyle@rose-hulman.edu>
2018-09-24 10:48:49 -0400

    Woot woot!

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -2,9 +2,28 @@
 A   Baby   class and methods that use the Baby class.
 
 Authors: David Mutchler, Vibha Alangar, Dave Fisher, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Lauren Smiley.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
+class Baby:
+    def __init__(self, name):
+        self.name = name
+        self.count = 0
+        print('Hello baby ' + name +'!')
+
+
+    def feed_baby(self):
+        print('Thank you for feeding baby ' + self.name + '.')
+        self.count = 0
+
+    def hour_passes(self):
+        if self.count == 0:
+            print('Baby ' + self.name + ' is sleeping.')
+        if self.count == 1:
+            print('Baby ' + self.name + ' is awake.  Time for food.')
+        if self.count >= 2:
+            print('Baby ' + self.name + ' is CRYING uncontrollably!  Feed the Baby!')
+        self.count = self.count + 1
 
 def main():
     """
@@ -39,7 +58,7 @@ def main():
 
 
 # ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
+# Done: 2. Implement a class called   Baby   that has a constructor and
 #   two methods, as described below.  Your finished Baby class should
 #   cause the code above to display the expected output.  Hint: Your
 #   class will need instance variables that you must figure out.
